2dd8eb08ef29838dde7a63af0a5b2cf468d8bdd5
sunny-toffee <sunny_toffee@mail.ru>
2015-09-06 19:54:44 +0300

    Hello Java!

---
+++ src/home/Test.java
@@ -0,0 +1,10 @@
+package home;
+
+/**
+ * Created by User on 06.09.2015.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello Java!");
+    }
+}
